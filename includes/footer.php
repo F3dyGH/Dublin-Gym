f96db3820fd16db1c23c2a3cbaff05077df75f9e
Fedi Said Ghourabi <47000166+Fedi-Said-Ghourabi@users.noreply.github.com>
2020-05-05 01:35:25 +0200

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -4,14 +4,14 @@
 					<div class="row">
 						<div class="col-md-4 animate-box">
 							<h3 class="section-title">About Us</h3>
-							<p>سلام عليكم ناسي و احبابي جملة و بالحملة</p>
-                            <h4 style="text-align: center; color: red;"> صلي عالنبي و يزي مالبلادة</h4>
+							<p>California Gym, The land of fitness,<br> is the First of fitness clubs in Tunisia to be certified by LesMills, the world's No.1 fitness concept.</p>
+                            <h4 style="text-align: center; color: #4CB648;"> Fitness & Health is a Mentality</h4>
 						</div>
 
 						<div class="col-md-4 animate-box">
 							<h3 class="section-title">Our Address</h3>
 							<ul class="contact-info">
-								<li><i class="icon-map-marker"></i>شقة عدد 5 عمارة سمير الوصيف , نهج صالح الفرزيط , حي الدرابك , تونس</li>
+								<li><i class="icon-map-marker"></i>Rue du Lac Biwa, Les Berges du Lac 1053 Tunis</li>
 								<li><i class="icon-phone"></i>+216 71 756 243</li>
 								<li><i class="icon-envelope"></i><a href="#">info@dublingym.com</a></li>
 								<li><i class="icon-globe2"></i><a href="#">www.dublingym.com</a></li>
