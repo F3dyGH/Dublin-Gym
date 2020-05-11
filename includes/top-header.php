91f18e2a1a93587fdd09126eb01085d8e1f94922
Fedi Said Ghourabi <47000166+Fedi-Said-Ghourabi@users.noreply.github.com>
2020-05-11 02:16:14 +0100

    Update top-header.php

--- includes/top-header.php
+++ includes/top-header.php
@@ -23,7 +23,7 @@
                     <li ><a href="my-account.php"><i class="icon fa fa-user"></i> My Account</a></li>
 					<li><a href="my-wishlist.php"><i class="icon fa fa-heart"></i> Wishlist</a></li>
 					<li><a href="my-cart.php"><i class="icon fa fa-shopping-cart"></i> My Cart</a></li>
-					<li><a href="order-history.php"><i class="icon fa fa-dashboard"></i> Order History</a></li>
+					<li><a href="order-history.php"><i class="icon fa fa-history"></i> Order History</a></li>
                     <li><a href="logout.php"><i class="icon fa fa-sign-out"></i> Logout</a></li>
                     </ul>
                     <div class="cnt-block">
