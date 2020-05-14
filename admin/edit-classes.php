8de5fbe87042d22602076f7aa9b9b2de6d5a6457
Fedi Said Ghourabi <47000166+Fedi-Said-Ghourabi@users.noreply.github.com>
2020-05-14 18:47:01 +0100

    Added Classes Managment to Back-end
    Co-Authored-By: mohamedazizgh <mohamedazizgh@users.noreply.github.com>

--- admin/edit-classes.php
+++ admin/edit-classes.php
@@ -10,8 +10,20 @@ else{
 date_default_timezone_set('Africa/Tunis');// change according timezone
 $currentTime = date( 'd-m-Y h:i:s A', time () );
 
+	$clid=intval($_GET['id']);// product id
+if(isset($_POST['update']))
+{
+	$personnel=$_POST['personnel'];
+	$nomcour=$_POST['nomcour'];
+	$jour=$_POST['jour'];
+	$start=$_POST['start'];
+	$end=$_POST['end'];
+	$description=$_POST['description'];
+	
+$sql=mysqli_query($con,"update  classes set personnel='$personnel',nomcour='$nomcour',jour='$jour',start='$start',end='$end',description='$description' where id='$clid' ");
+$_SESSION['msg']="Class Updated Successfully !!";
 
-//Requete SQl Update lel table cours w coach
+}
 
 ?>
 <!DOCTYPE html>
@@ -42,7 +54,95 @@ $currentTime = date( 'd-m-Y h:i:s A', time () );
 							</div>
 							<div class="module-body">
 
-									<!-- Form taa l modification lel attributs taa table cours w coach -->
+									<?php if(isset($_POST['update']))
+{?>
+									<div class="alert alert-success">
+										<button type="button" class="close" data-dismiss="alert">×</button>
+									<strong>Well done!</strong>	<?php echo htmlentities($_SESSION['msg']);?><?php echo htmlentities($_SESSION['msg']="");?>
+									</div>
+<?php } ?>
+
+
+									<?php if(isset($_GET['del']))
+{?>
+									<div class="alert alert-error">
+										<button type="button" class="close" data-dismiss="alert">×</button>
+									<strong>Oh snap!</strong> 	<?php echo htmlentities($_SESSION['delmsg']);?><?php echo htmlentities($_SESSION['delmsg']="");?>
+									</div>
+<?php } ?>
+
+									<br />	
+                                
+                                <form class="form-horizontal row-fluid" name="insertproduct" method="post" enctype="multipart/form-data">
+
+<div class="control-group">
+<label class="control-label" for="basicinput">Trainer</label>
+<div class="controls">
+<select name="personnel" class="span8 tip"  required>
+<option value="">Select Trainer</option> 
+<?php $query=mysqli_query($con,"select * from personnel");
+while($row=mysqli_fetch_array($query))
+{?>
+
+<option value="<?php echo $row['nom']; ?>  <?php echo $row['prenom'];?>"><?php echo $row['nom']; ?>  <?php echo $row['prenom'];?></option>
+<?php } ?>
+</select>
+</div>
+</div>
+
+									
+
+
+<div class="control-group">
+<label class="control-label" for="basicinput">Class Name</label>
+<div class="controls">
+<input type="text"    name="nomcour"  placeholder="Enter Class Name" class="span8 tip" required>
+</div>
+</div>
+
+<div class="control-group">
+<label class="control-label" for="basicinput">Week Day</label>
+<div class="controls">
+<select   name="jour"  id="jour" class="span8 tip" required>
+<option value="">Select</option>
+<option value="Monday">Monday</option>
+<option value="Tuesday">Tuesday</option>
+<option value="Wednesday">Wednesday</option>
+<option value="Thursday">Thursday</option>
+<option value="Friday">Friday</option>
+<option value="Saturday">Saturday</option>
+<option value="Sunday">Sunday</option>
+</select>
+</div>
+</div>
+<div class="control-group">
+<label class="control-label" for="basicinput">Starting Hour</label>
+<div class="controls">
+<input type="text" name="start"  placeholder="Enter Starting Hour" class="span8 tip" >
+</div>
+</div>
+
+<div class="control-group">
+<label class="control-label" for="basicinput">Ending Hour</label>
+<div class="controls">
+<input type="text"    name="end"  placeholder="Enter Ending Hour" class="span8 tip" required>
+</div>
+</div>
+
+<div class="control-group">
+<label class="control-label" for="basicinput">Description</label>
+<div class="controls">
+<textarea  name="description"  placeholder="Enter Class Description" rows="6" class="span8 tip">
+</textarea>  
+</div>
+</div>
+                                    <div class="control-group">
+											<div class="controls">
+												<button type="submit" name="update" class="btn">Update</button>
+											</div>
+										</div>
+
+                                </form>
 				
 							</div>
 						</div>
